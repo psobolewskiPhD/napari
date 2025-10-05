d9b77936311a55405aff945d801bd4dd8fe41810
Peter Sobolewski <76622105+psobolewskiPhD@users.noreply.github.com>
2025-10-05 12:00:37 -0400

    fix sorting?

--- src/napari/_tests/test_viewer.py
+++ src/napari/_tests/test_viewer.py
@@ -28,7 +28,7 @@ def _get_provider_actions(type_):
                 superclass
             ).values()
         )
-    return actions
+    return sorted(actions, key=lambda action: action.__name__)
 
 
 def _assert_shortcuts_exist_for_each_action(type_):
@@ -43,7 +43,7 @@ def _assert_shortcuts_exist_for_each_action(type_):
         )
 
 
-viewer_actions = sorted(_get_provider_actions(Viewer), key=lambda action: action.name)
+viewer_actions = _get_provider_actions(Viewer)
 
 
 def test_all_viewer_actions_are_accessible_via_shortcut(make_napari_viewer):
