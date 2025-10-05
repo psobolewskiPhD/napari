b04d9ad5bdf8e700b0a9aa9aa8b2138d9a095105
Peter Sobolewski <76622105+psobolewskiPhD@users.noreply.github.com>
2025-10-05 10:58:27 -0400

    sort actions for reproducibility

--- src/napari/_tests/test_viewer.py
+++ src/napari/_tests/test_viewer.py
@@ -43,7 +43,7 @@ def _assert_shortcuts_exist_for_each_action(type_):
         )
 
 
-viewer_actions = _get_provider_actions(Viewer)
+viewer_actions = sorted(_get_provider_actions(Viewer))
 
 
 def test_all_viewer_actions_are_accessible_via_shortcut(make_napari_viewer):
