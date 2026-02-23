psobolewskiPhD/napari

commit 12b031b0482981835a4b025a0c0efc6a4053d089

Add/unify background box for canvas overlays (#8654)
# References and relevant issues
previous discussions:
- https://github.com/napari/napari/issues/8612
- https://github.com/napari/napari/issues/8648

# Description
This PR adds a `box` and `box_color` field to *all* canvas overlays,
following the pattern from `scale_bar` (which is now using this new
method), and with similar behaviour.

Note that because our overlays are still not great at reporting their
own size (due to `Text` being nasty), this can look a bit meh in some
cases, but in general looks better than before IMO.


```py
import napari
v = napari.Viewer()
v.scale_bar.visible = True
ll = v.open_sample('napari', 'lily')
for l in ll:
    l.colorbar.visible = True
```

<img width="1102" height="747" alt="image"
src="https://github.com/user-attachments/assets/dd6a56df-1998-430f-b4a7-b5ff20313a77"
/>

---------

Co-authored-by: Grzegorz Bokota <bokota+github@gmail.com>
Co-authored-by: Tim Monko <timmonko@gmail.com>
Co-authored-by: Draga Doncila Pop <17995243+DragaDoncila@users.noreply.github.com>
Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

src/napari/_vispy/overlays/base.py
@@ -2,13 +2,19 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import numpy as np
+from vispy.scene.visuals import Rectangle
 from vispy.visuals.transforms import MatrixTransform, STTransform
 
 from napari._vispy.utils.gl import BLENDING_MODES
-from napari.components.overlays import Overlay
+from napari.settings import get_settings
+from napari.utils.color import ColorValue
 from napari.utils.events import disconnect_events
+from napari.utils.theme import get_theme
 
 if TYPE_CHECKING:
+    from napari.components.overlays import CanvasOverlay, Overlay, SceneOverlay
+    from napari.components.viewer_model import ViewerModel
     from napari.layers import Layer
     from napari.utils.events import Event
 
@@ -21,9 +27,13 @@ class VispyBaseOverlay:
     vispy backend, translating them into rendering.
     """
 
-    def __init__(self, *, overlay, node, parent=None) -> None:
+    overlay: Overlay
+    viewer: ViewerModel
+
+    def __init__(self, *, overlay, viewer, node, parent=None) -> None:
         super().__init__()
-        self.overlay: Overlay = overlay
+        self.overlay = overlay
+        self.viewer = viewer
 
         self.node = node
         self.node.order = self.overlay.order
@@ -35,8 +45,11 @@ class VispyBaseOverlay:
         if parent is not None:
             self.node.parent = parent
 
+    def _should_be_visible(self) -> bool:
+        return self.overlay.visible
+
     def _on_visible_change(self) -> None:
-        self.node.visible = self.overlay.visible
+        self.node.visible = self._should_be_visible()
 
     def _on_opacity_change(self) -> None:
         self.node.opacity = self.overlay.opacity
@@ -54,6 +67,7 @@ class VispyBaseOverlay:
         self.overlay.events.visible.disconnect(self._on_visible_change)
         self.overlay.events.opacity.disconnect(self._on_opacity_change)
         self.overlay.events.blending.disconnect(self._on_blending_change)
+        disconnect_events(self.viewer.events, self)
         self.node.transforms = MatrixTransform()
         self.node.parent = None
 
@@ -74,17 +88,86 @@ class VispyCanvasOverlay(VispyBaseOverlay):
     to update the position of all canvas overlays whenever necessary
     """
 
-    def __init__(self, *, overlay, node, parent=None) -> None:
-        super().__init__(overlay=overlay, node=node, parent=parent)
+    overlay: CanvasOverlay
+
+    def __init__(self, *, overlay, viewer, node, parent=None) -> None:
+
+        super().__init__(
+            overlay=overlay, viewer=viewer, node=node, parent=parent
+        )
         self.x_size = 0.0
         self.y_size = 0.0
         self.node.transform = STTransform()
         self.overlay.events.position.connect(self._on_position_change)
+        self.overlay.events.box.connect(self._on_box_change)
+        self.overlay.events.box_color.connect(self._on_box_change)
+        get_settings().appearance.events.theme.connect(self._on_box_change)
+        self.viewer.events.theme.connect(self._on_box_change)
         self.canvas_position_callback = lambda: None
+
+        self.box = Rectangle(center=(0, 0), border_width=0)
+
+    def _on_visible_change(self) -> None:
+        super()._on_visible_change()
+        self._on_box_change()
+
+    def _on_box_change(self) -> None:
+        if not self.overlay.box or not self.node.visible:
+            self.box.parent = None
+            return
+
+        self.box.parent = self.node.parent
+
+        # TODO: this should be related to tiling padding
+        padding = 8
+        self.box.width = self.x_size + padding
+        self.box.height = self.y_size + padding
+        self.box.center = self.x_size / 2, self.y_size / 2
+
+        if self.overlay.box_color is None:
+            bgcolor = self._get_canvas_bgcolor()
+            # make the color a bit transparent
+            bgcolor[-1] *= 0.8
+        else:
+            bgcolor = self.overlay.box_color
+
+        self.box.color = bgcolor
+
+        self.box.order = self.node.order - 1
+        self.box.transform = self.node.transform
+
+    def _get_canvas_bgcolor(self) -> ColorValue:
+        if self.node.parent is not None and self.node.parent.canvas.bgcolor:
+            return ColorValue(self.node.parent.canvas.bgcolor.rgba)
+
+        return ColorValue(
+            get_theme(get_settings().appearance.theme).canvas.as_rgb_tuple()
+        )
+
+    def _get_fgcolor(self) -> ColorValue:
+        if not self.overlay.box or self.overlay.box_color is None:
+            bgcolor = self._get_canvas_bgcolor()
+        else:
+            bgcolor = self.overlay.box_color
+        return self._contrasting_color(bgcolor)
+
+    def _contrasting_color(self, bgcolor: ColorValue) -> ColorValue:
+        opposite = 1 - bgcolor
+        # shift away from mid tones for better contrast
+        opposite = 0.5 + (opposite - 0.5) * 1.2
+        opposite = np.clip(opposite, 0, 1)
+        # don't change alpha
+        opposite[-1] = bgcolor[-1]
+        return opposite
+
+    def _on_blending_change(self) -> None:
+        self.box.set_gl_state(**BLENDING_MODES[self.overlay.blending])
+        super()._on_blending_change()
 
     def _on_position_change(self, event: Event | None = None) -> None:
         # NOTE: when subclasses call this method, they should first ensure sizes
         # (x_size, and y_size) are set correctly
+        self._on_box_change()
         self.canvas_position_callback()
 
     def reset(self) -> None:
@@ -93,6 +176,7 @@ class VispyCanvasOverlay(VispyBaseOverlay):
 
     def close(self) -> None:
         super().close()
+        self.box.parent = None
         self.canvas_position_callback = lambda: None
 
 
@@ -101,16 +185,25 @@ class VispySceneOverlay(VispyBaseOverlay):
     Vispy overlay backend for overlays that live in scene (2D or 3D) space.
     """
 
-    def __init__(self, *, overlay, node, parent=None) -> None:
-        super().__init__(overlay=overlay, node=node, parent=parent)
+    overlay: SceneOverlay
+
+    def __init__(self, *, overlay, viewer, node, parent=None) -> None:
+        super().__init__(
+            overlay=overlay, viewer=viewer, node=node, parent=parent
+        )
         self.node.transform = MatrixTransform()
 
 
 class LayerOverlayMixin:
-    def __init__(self, *, layer: Layer, overlay, node, parent=None) -> None:
+    layer: Layer
+
+    def __init__(
+        self, *, overlay, layer: Layer, viewer, node, parent=None
+    ) -> None:
         super().__init__(
             node=node,
             overlay=overlay,
+            viewer=viewer,
             parent=parent,
         )
         self.layer = layer
@@ -118,8 +211,8 @@ class LayerOverlayMixin:
         # always a child of the actual vispy node of the layer (eg, canvas overlays)
         self.layer.events.visible.connect(self._on_visible_change)
 
-    def _on_visible_change(self) -> None:
-        self.node.visible = self.overlay.visible and self.layer.visible
+    def _should_be_visible(self) -> bool:
+        return self.overlay.visible and self.layer.visible
 
     def close(self) -> None:
         disconnect_events(self.layer.events, self)
@@ -127,14 +220,4 @@ class LayerOverlayMixin:
 
 
 class ViewerOverlayMixin:
-    def __init__(self, *, viewer, overlay, node, parent=None) -> None:
-        super().__init__(
-            node=node,
-            overlay=overlay,
-            parent=parent,
-        )
-        self.viewer = viewer
-
-    def close(self) -> None:
-        disconnect_events(self.viewer.events, self)
-        super().close()
+    pass

src/napari/_vispy/overlays/welcome.py
@@ -6,11 +6,11 @@ from typing import TYPE_CHECKING, Any
 
 import numpy as np
 from vispy.app.timer import Timer
+from vispy.visuals.transforms import NullTransform
 
 from napari._vispy.overlays.base import ViewerOverlayMixin, VispyCanvasOverlay
 from napari._vispy.visuals.welcome import Welcome
-from napari.utils.colormaps.standardize_color import transform_color
-from napari.utils.theme import get_theme
+from napari.settings import get_settings
 
 if TYPE_CHECKING:
     from vispy.scene import Node
@@ -37,9 +37,13 @@ class VispyWelcomeOverlay(ViewerOverlayMixin, VispyCanvasOverlay):
         self.viewer.layers.events.inserted.connect(self._on_visible_change)
         self.viewer.layers.events.removed.connect(self._on_visible_change)
 
+        get_settings().appearance.events.theme.connect(self._on_theme_change)
+
         self.overlay.events.version.connect(self._on_version_change)
         self.overlay.events.shortcuts.connect(self._on_shortcuts_change)
         self.overlay.events.tips.connect(self._on_tips_change)
+        self.overlay.events.box.connect(self._on_theme_change)
+        self.overlay.events.box_color.connect(self._on_theme_change)
 
         self.node.canvas.native.resized.connect(self._on_position_change)
 
@@ -53,21 +57,26 @@ class VispyWelcomeOverlay(ViewerOverlayMixin, VispyCanvasOverlay):
         if self.node.canvas is not None:
             x, y = np.array(self.node.canvas.size)
             self.node.set_scale_and_position(x, y)
+            self.x_size = x
+            self.y_size = y
+        self._on_box_change()
+
+    def _on_box_change(self) -> None:
+        super()._on_box_change()
+        # welcome uses some custom positioning with the transform, so copying it
+        # over messes it up. We just set it to nothing.
+        self.box.transform = NullTransform()
+        # always opaque box color, so we hide what's behind
+        bgcolor = self.box.color.rgba
+        bgcolor[-1] = 1
+        self.box.color = bgcolor
 
     def _on_theme_change(self) -> None:
-        if self.node.parent is not None and self.node.parent.canvas.bgcolor:
-            background_color = self.node.parent.canvas.bgcolor.rgba
-        else:
-            background_color = get_theme(self.viewer.theme).canvas.as_hex()
-            background_color = transform_color(background_color)[0]
-
-        # ensure background is opaque
-        background_color[-1] = 1.0
-
-        color = np.subtract(1, background_color)
-        color[-1] = background_color[-1]
-        color *= 0.8  # dim a bit
-        self.node.set_color(color, background_color)
+        color = self._get_fgcolor()
+        # dim a bit but keep opaque to avoid border artifacts
+        color[:3] *= 0.7
+        color[-1] = 1
+        self.node.set_color(color)
 
     def _on_visible_change(self) -> None:
         show = self.overlay.visible and not self.viewer.layers

src/napari/_vispy/visuals/welcome.py
@@ -8,7 +8,7 @@ from typing import TYPE_CHECKING, Any
 
 import numpy as np
 from vispy.scene.node import Node
-from vispy.scene.visuals import Polygon, Rectangle
+from vispy.scene.visuals import Polygon
 from vispy.util.svg import Document
 from vispy.visuals.transforms import STTransform
 
@@ -46,7 +46,6 @@ class Welcome(Node):
         self.logo_coords[:, 1] -= 130  # magic number shifting up logo
         super().__init__()
 
-        self.bg = Rectangle(center=[0, 0], parent=self, border_width=0)
         self.logo = Polygon(
             self.logo_coords, border_method='agg', border_width=2, parent=self
         )
@@ -88,8 +87,7 @@ class Welcome(Node):
 
         self.transform = STTransform()
 
-    def set_color(self, color: ColorValue, bg_color: ColorValue) -> None:
-        self.bg.color = bg_color
+    def set_color(self, color: ColorValue) -> None:
         self.logo.color = color
         self.logo.border_color = color
         self.header.color = color
@@ -162,9 +160,6 @@ class Welcome(Node):
         self.transform.translate = (x / 2, y / 2, 0, 0)
         scale = min(x, y) * 0.002  # magic number
         self.transform.scale = (scale, scale, 0, 0)
-
-        self.bg.width = x / scale
-        self.bg.height = y / scale
 
         for text in (
             self.header,

src/napari/components/overlays/base.py
@@ -5,6 +5,7 @@ from pydantic import ConfigDict
 
 from napari.components._viewer_constants import CanvasPosition
 from napari.layers.base._base_constants import Blending
+from napari.utils.color import ColorValue
 
 
 class Overlay(EventedModel):
@@ -61,6 +62,8 @@ class CanvasOverlay(Overlay):
 
     position: CanvasPosition | Any = CanvasPosition.BOTTOM_RIGHT
     blending: Blending = Blending.TRANSLUCENT_NO_DEPTH
+    box: bool = True
+    box_color: ColorValue | None = None
     gridded: bool = False
 
 

src/napari/components/overlays/welcome.py
@@ -10,7 +10,7 @@ class WelcomeOverlay(CanvasOverlay):
     # not settable in this specific overlay
     position: None = None
     # ensure it's on top of overlays with default value
-    order: int = 10**6 + 1
+    order: int = 10**6 + 10
     gridded: Literal[False] = False
     version: str = __version__
     shortcuts: tuple[str, ...] = (

pyproject.toml
@@ -55,7 +55,7 @@ dependencies = [
     "npe2>=0.8.1",
     "numpy>=1.24.2",
     "pandas>=1.3.3",
-    "Pillow>=9.0",
+    "Pillow>=10.0",
     "pint>=0.17",
     "psutil>=5.9.0",
     "psygnal>=0.14.2",

src/napari/_vispy/_tests/test_vispy_image_layer.py
@@ -9,6 +9,7 @@ from napari._vispy.layers.image import VispyImageLayer
 from napari._vispy.utils.visual import create_vispy_overlay
 from napari.components.dims import Dims
 from napari.components.overlays import BoundingBoxOverlay
+from napari.components.viewer_model import ViewerModel
 from napari.layers import Image
 
 
@@ -148,7 +149,10 @@ def test_transforming_child_node(
     layer = VispyImageLayer(im_layer)
 
     overlay = create_vispy_overlay(
-        BoundingBoxOverlay(), layer=im_layer, parent=layer.node
+        BoundingBoxOverlay(),
+        layer=im_layer,
+        viewer=ViewerModel(),
+        parent=layer.node,
     )
     layer._on_matrix_change()
 
@@ -189,7 +193,10 @@ def test_transforming_child_node_pyramid(pyramid_layer):
     layer = VispyImageLayer(pyramid_layer)
 
     overlay = create_vispy_overlay(
-        BoundingBoxOverlay(), layer=pyramid_layer, parent=layer.node
+        BoundingBoxOverlay(),
+        layer=pyramid_layer,
+        viewer=ViewerModel(),
+        parent=layer.node,
     )
     layer._on_matrix_change()
 

src/napari/_vispy/_tests/test_vispy_labels_polygon_overlay.py
@@ -18,7 +18,7 @@ def test_vispy_labels_polygon_overlay(make_napari_viewer):
     layer = viewer.add_labels(data, opacity=0.5)
 
     vispy_labels_polygon = VispyLabelsPolygonOverlay(
-        layer=layer, overlay=labels_polygon
+        layer=layer, viewer=viewer, overlay=labels_polygon
     )
 
     assert vispy_labels_polygon._polygon.color.alpha == 0.5

commit bb0455f2ebdfe1b42a955fcd23b9d8aa1e23a0eb

Correctly hide welcome screen when layers are present (#8688)
# References and relevant issues
- caused by #8654

# Description
The new overlay boxes visibility can sometimes be out of sync with the
overlay itself. Try this *in two separate ipython cells*:

```py
import napari
import numpy as np
v = napari.Viewer()
```

```py
v.add_points(np.random.rand(10, 2) * 100)
```

You'll see a black screen instead of points, because the
`welcome_screen`'s box remained visible. This PR just cleans up this by
always using `_should_be_visible` when appropriate.

src/napari/_vispy/overlays/base.py
@@ -112,7 +112,7 @@ class VispyCanvasOverlay(VispyBaseOverlay):
         self._on_box_change()
 
     def _on_box_change(self) -> None:
-        if not self.overlay.box or not self.node.visible:
+        if not self.overlay.box or not self._should_be_visible():
             self.box.parent = None
             return
 

src/napari/_vispy/overlays/welcome.py
@@ -53,6 +53,9 @@ class VispyWelcomeOverlay(ViewerOverlayMixin, VispyCanvasOverlay):
 
         self.reset()
 
+    def _should_be_visible(self) -> bool:
+        return bool(super()._should_be_visible() and not self.viewer.layers)
+
     def _on_position_change(self, event: Any = None) -> None:
         if self.node.canvas is not None:
             x, y = np.array(self.node.canvas.size)
@@ -79,7 +82,7 @@ class VispyWelcomeOverlay(ViewerOverlayMixin, VispyCanvasOverlay):
         self.node.set_color(color)
 
     def _on_visible_change(self) -> None:
-        show = self.overlay.visible and not self.viewer.layers
+        show = self._should_be_visible()
         self.node.visible = show
         if show:
             self.tip_timer.start()